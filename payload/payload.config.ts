8e65241c652d408fcf29cd27f9b7dcfe21d97289
Leo <leo.hilsheimer@gmail.com>
2024-08-30 20:08:43 +0200

    add PayloadCMS w/o admin panel (local API only)

---
+++ payload/payload.config.ts
@@ -0,0 +1,26 @@
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import { fileURLToPath } from "url";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  editor: lexicalEditor(),
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || "",
+  }),
+  collections: [
+    {
+      slug: "users",
+      auth: true,
+      fields: [],
+    },
+  ],
+  secret: process.env.PAYLOAD_SECRET ?? "",
+  typescript: {
+    outputFile: path.resolve(dirname, "payload-types.ts"),
+  },
+});
